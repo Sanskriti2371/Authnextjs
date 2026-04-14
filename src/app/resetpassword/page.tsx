b733258b8a484253345805f6a42f1ef520d1ecb8
Sanskriti Mishra <sanskritimishra2611@gmail.com>
2026-04-14 15:43:18 +0530

    fixed

--- src/app/resetpassword/page.tsx
+++ src/app/resetpassword/page.tsx
@@ -1,5 +1,8 @@
+
+"use client";
+
 export const dynamic = "force-dynamic";
-("use client");
+
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
