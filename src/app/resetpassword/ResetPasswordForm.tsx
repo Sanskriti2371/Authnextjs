21eb152553c31e76b64d5c580fc9e8c27af8c189
Sanskriti Mishra <sanskritimishra2611@gmail.com>
2026-04-14 15:49:12 +0530

    fixed issue

---
+++ src/app/resetpassword/ResetPasswordForm.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+export default function ResetPasswordForm() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const [password, setPassword] = useState("");
+
+  const handleReset = async () => {
+    try {
+      await axios.post("/api/users/resetpassword", {
+        token,
+        password,
+      });
+      alert("Password reset successful");
+    } catch (error: any) {
+      alert(error.response.data.error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Reset Password</h1>
+      <input
+        type="password"
+        placeholder="Enter new password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleReset}>Submit</button>
+    </div>
+  );
+}
